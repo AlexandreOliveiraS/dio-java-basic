708e55bb686ae1c7c1ede8866cdbab6b777d2ab1
AlexandreOliveiraS <alexandreoliveiradsantos244@gmail.com>
2023-08-31 22:04:35 -0300

    Changed

--- java-inicio/src/MinhaClasse.java
+++ java-inicio/src/MinhaClasse.java
@@ -1,6 +1,7 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.println("Olá, Seja Bem Vindo!!!");
+        System.out.println("Vou ser um DEV muito FODA!!!");
+        System.out.println("Ninguém vai me Parar, se Deus Disse que Posso. ENTÃO EU POSSO!");
     }
 }
