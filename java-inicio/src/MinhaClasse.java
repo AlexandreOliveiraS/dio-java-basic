0eef150c96939a43cd344f0e31e61a35836859b7
AlexandreOliveiraS <alexandreoliveiradsantos244@gmail.com>
2023-09-09 07:02:36 -0300

    Rumo aos projetos

--- java-inicio/src/MinhaClasse.java
+++ java-inicio/src/MinhaClasse.java
@@ -1,6 +1,16 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.println("Vou ser um DEV muito FODA!!!");
+
+        String primeiroNome = "Alexandre";
+        String segundoNome = "Oliveira";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
